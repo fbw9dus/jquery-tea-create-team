98e0e1ef4fb4d16d12fd41a8042ebab560a72467
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2019-12-09 12:25:52 +0100

    Abdelhamed

--- index.js
+++ index.js
@@ -8,11 +8,22 @@ $('#submit').on('click',event=>{
 
 $('#container').on('click',event=> {
     
-    $('#container').hide()
-    $('#message').stopPropagation()
+    
+    if(event.target === $('#container')[0]) {$('#container').hide()}
     
 })
 
 $('#closeBtn').on('click',event=>{
     $('#container').hide()
 })
+
+$('#image').mouseenter(event => {
+      $('#image').css('transform','rotate(20deg)');
+    },
+    
+)
+$('#image').mouseleave(event => {
+    $('#image').css('transform','rotate(0deg)');
+  },
+  
+)
