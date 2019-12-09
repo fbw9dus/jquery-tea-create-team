ec50dd5291bc8b20c76c89623d07d05d7d5648a4
abdelhamed alomar <abdelhamed.alomar.1996@gmail.com>
2019-12-09 11:39:54 +0100

    Abdelhamed

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Work on your tasks here
+
+
+$('#submit').on('click',event=>{
+    $('#container').css('display','flex')
+    
+
+})
+
+$('#container').on('click',event=> {
+    
+    $('#container').hide()
+    $('#message').stopPropagation()
+    
+})
+
+$('#closeBtn').on('click',event=>{
+    $('#container').hide()
+})
